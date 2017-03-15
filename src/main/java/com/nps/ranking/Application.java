6e2875a8f30bb2bb9cd2f15a0426757898f8c459
Norbert Pabina <norbert.pabian@gmail.com>
2017-03-16 00:51:48 +0100

    Initial setup

---
+++ src/main/java/com/nps/ranking/Application.java
@@ -0,0 +1,15 @@
+package com.nps.ranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Norbert Pabian on 16.03.17
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
